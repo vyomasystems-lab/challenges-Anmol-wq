b4eb53997c87b30dff2521a5dcba9a6347836165
Anmol Saxena <anmol.saxena2016@outlook.com>
2022-07-14 12:35:05 +0000

    another experiment

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,16 +6,17 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
-    cocotb.log.info('##### CTB: Develop your test here ########')
-
-    
-
     dut.sel.value = 13
 #    await Timer(1, units="ns")
     dut.inp12.value = 2
 #    await Timer(1, units="ns")
     dut.inp13.value = 3
     await Timer(1, units="ns")
+    cocotb.log.info('##### CTB: Develop your test here ########')
+
+    
+
+    
     dut._log.info("my out is %s", dut.out.value)
     assert dut.out.value == dut.inp12.value, "could be inp13 because same sel bits assigned for both"
     assert dut.out.value == dut.inp13.value, "could be inp12 because same sel bits assigned for both"
