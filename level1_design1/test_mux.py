92c303aaf7c63ed76ce88fb332efbbdf81f6e5fd
Anmol Saxena <anmol.saxena2016@outlook.com>
2022-07-14 06:11:17 +0000

    rectifying errors_1

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -8,21 +8,19 @@ async def test_mux(dut):
     """Test for mux2"""
     cocotb.log.info('##### CTB: Develop your test here ########')
 
-      sel=5'b01101
-      inp12=2'b11
-      inp13=2'b01
+    
 
-      dut.sel.value = sel
-      await Timer(1, units="ns")
-      dut.inp12.value = inp12
-      await Timer(1, units="ns")
-      dut.inp13.value = inp13
-      await Timer(1, units="ns")
-      dut.inp12.value = inp12
-      await Timer(1, units="ns")
-      dut._log.info("my out is %s", dut.out.value)
-      assert dut.out.value == dut.inp12.value, "could be sel13 because same sel bits assigned for both"
-      assert dut.out.value == dut.inp13.value, "could be sel12 because same sel bits assigned for both"
+    dut.sel.value = "01101"
+    await Timer(1, units="ns")
+    dut.inp12.value = "01"
+    await Timer(1, units="ns")
+    dut.inp13.value = "11"
+    await Timer(1, units="ns")
+    dut.inp12.value = "00"
+    await Timer(1, units="ns")
+    dut._log.info("my out is %s", dut.out.value)
+    assert dut.out.value == dut.inp12.value, "could be sel13 because same sel bits assigned for both"
+    assert dut.out.value == dut.inp13.value, "could be sel12 because same sel bits assigned for both"
 
 
       
