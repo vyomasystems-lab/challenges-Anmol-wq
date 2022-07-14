3ecd384499462098e1d0871c3681a2281397eaa7
Anmol Saxena <anmol.saxena2016@outlook.com>
2022-07-14 06:17:38 +0000

    minor assert string correction from sel to inp

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -17,8 +17,8 @@ async def test_mux(dut):
     dut.inp13.value = 3
     await Timer(1, units="ns")
     dut._log.info("my out is %s", dut.out.value)
-    assert dut.out.value == dut.inp12.value, "could be sel13 because same sel bits assigned for both"
-    assert dut.out.value == dut.inp13.value, "could be sel12 because same sel bits assigned for both"
+    assert dut.out.value == dut.inp12.value, "could be inp13 because same sel bits assigned for both"
+    assert dut.out.value == dut.inp13.value, "could be inp12 because same sel bits assigned for both"
 
 
       
