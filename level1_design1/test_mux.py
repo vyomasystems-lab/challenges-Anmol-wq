6f035439f72eab52fc9a697975c181e0d4d1ec02
Anmol Saxena <anmol.saxena2016@outlook.com>
2022-07-31 07:27:20 +0000

    updated test file

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -2,27 +2,55 @@
 
 import cocotb
 from cocotb.triggers import Timer
+import random
 
 @cocotb.test()
-async def test_mux(dutt):
+async def test_mux(dut):
     """Test for mux2"""
-    dut.sel.value = 13
-#    await Timer(1, units="ns")
-    dut.inp12.value = 2
-#    await Timer(1, units="ns")
-    dut.inp13.value = 3
-    await Timer(1, units="ns")
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    for i in range(31):
+        
+            in0 = random.randint(0, 3)
+            in1 = random.randint(0, 3)
+            in2 = random.randint(0, 3)
+            in3 = random.randint(0, 3)
+            in4 = random.randint(0, 3)
+            in5 = random.randint(0, 3)
+            in6 = random.randint(0, 3)
+            in7 = random.randint(0, 3)
+            in8 = random.randint(0, 3)
+            in9 = random.randint(0, 3)
+            in10 = random.randint(0, 3)
+            in11 = random.randint(0, 3)
+            in12 = random.randint(0, 3)
+            in13= random.randint(0, 3)
+            in14= random.randint(0, 3)
+            in15= random.randint(0, 3)
+            
 
-    
+            input = [in0, in1, in2, in3, in4, in5, in6, in7, in8, 
+            in9, in10, in11, in12, in13, in14, in15]
 
-    
-    dut._log.info("my out is %s", dut.out.value)
-    assert dut.out.value == dut.inp12.value, "could be inp13 because same sel bits assigned for both"
-    assert dut.out.value == dut.inp13.value, "could be inp12 because same sel bits assigned for both"
+            dut.inp0.value = in0
+            dut.inp1.value = in1
+            dut.inp2.value = in2
+            dut.inp3.value = in3
+            dut.inp4.value = in4
+            dut.inp5.value = in5
+            dut.inp6.value = in6
+            dut.inp7.value = in7
+            dut.inp8.value = in8
+            dut.inp9.value = in9
+            dut.inp10.value = in10
+            dut.inp11.value = in11
+            dut.inp12.value = in12
+            dut.inp13.value = in13
+            dut.inp14.value = in14
+            dut.inp15.value = in15
+            
+            dut.sel.value = i
 
+            await Timer(2, units='ns')
 
-      
-
-      
-    
+            dut._log.info(f'Selection={i} model={int(input[i])} DUT={int(dut.out.value)}')
+            #cocotb.log.info('##### CTB: Develop your test here ########')
+            assert dut.out.value == input[i], f"Randomised test failed with: Selction {dut.sel.value} and corrsponding inputline {input[i]} and output {dut.out.value}"
